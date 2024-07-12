19420fe752a9d708507016338b19bc88a11c0fab
RaghunagaY <yrnp2355@gmail.com>
2024-07-12 14:23:03 +0530

    AMI ID updated

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb" #devops-practice
+  ami           = "ami-0b4f379183e5706b9" #devops-practice
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
 
--- variables/variables.tf
+++ variables/variables.tf
@@ -4,7 +4,7 @@
 # 4. ENV variables
 variable "ami_id" {
   type = string
-  default = "ami-03265a0778a880afb"
+  default = "ami-0b4f379183e5706b9"
 }
 
 variable "instance_type" {
